2da8c9c7246e9810c1e1313174823091f1c7aaff
LndTrinity <leonardoperezdaluz@gmail.com>
2025-12-02 18:42:05 -0300

    correcao

--- src/routes/alerta_tipo.ts
+++ src/routes/alerta_tipo.ts
@@ -1,5 +1,5 @@
 import { error } from "console";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "@prisma/client";
 import { Router, Request, Response } from "express";
 
 const prisma = new PrismaClient();
